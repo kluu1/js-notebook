aada838f0a9b80f2117be62d3803c5adabaaff08
Kevin Luu <this.kevinluu@gmail.com>
2023-01-22 22:29:34 -0500

    cache packages

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
+Summary
+
 Technology used
 
 - React.js
-- ESBuild
-- Unpkg
+- ESBuild to transplie/compile js code
+- Unpkg to fetch npm packages
+- Localforage to cache npm packages in indexedDB
 
 - Write custom ESBuild plugin to pull packages from unpkg
+- Cache npm packages that's already been fetched in indexedDB
---
+++ src/plugins/fetch-plugin.ts
@@ -0,0 +1,71 @@
+import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
+import localforage from 'localforage';
+
+// setup our file cache to store our npm packages
+const fileCache = localforage.createInstance({
+  name: 'filecache',
+});
+
+export const fetchPlugin = (inputCode: string) => {
+  return {
+    name: 'fetch-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      });
+
+      // check if npm package is cached
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+      });
+
+      // handle css file: current work around because esbuild does not allow user to select output
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const contents = `
+            const style = document.createElement('style');
+            style.innerText = '${escaped}';
+            document.head.appendChild(style);
+          `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+    },
+  };
+};
