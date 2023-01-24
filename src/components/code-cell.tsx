c9298e5bb4055476caa6c2a5b78f50ac369201c8
Kevin Luu <this.kevinluu@gmail.com>
2023-01-24 08:52:54 -0500

    handle errors

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -6,12 +6,14 @@ import Resizeable from './resizable';
 
 const CodeCell = () => {
   const [code, setCode] = useState('');
+  const [err, setErr] = useState('');
   const [input, setInput] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setErr(output.err);
     }, 1000);
     // will run next time useEffect is called
     return () => clearTimeout(timer);
@@ -26,7 +28,7 @@ const CodeCell = () => {
             onChange={(value) => setInput(value)}
           />
         </Resizeable>
-        <Preview code={code} />
+        <Preview code={code} err={err} />
       </div>
     </Resizeable>
   );
