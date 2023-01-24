94b8873fa1c6a01cc31484a8a1db3981c1f9a849
Kevin Luu <this.kevinluu@gmail.com>
2023-01-23 21:04:55 -0500

    configure resizeable components

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,28 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import bundle from '../bundler';
+import Resizeable from './resizable';
 
 const CodeCell = () => {
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [input]);
 
   return (
-    <div>
-      <CodeEditor
-        initialValue="const a = 1"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
+    <Resizeable direction="vertical">
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
+        <Resizeable direction="horizontal">
+          <CodeEditor
+            initialValue="const a = 1"
+            onChange={(value) => setInput(value)}
+          />
+        </Resizeable>
+        <Preview code={code} />
       </div>
-      <Preview code={code} />
-    </div>
+    </Resizeable>
   );
 };
 
--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,3 +1,4 @@
+import './preview.css';
 import { useEffect, useRef } from 'react';
 
 interface PreviewProps {
@@ -6,7 +7,9 @@ interface PreviewProps {
 
 const html = `
     <html>
-      <head></head>
+      <head>
+        <style>html { background-color:white } </style>
+      </head>
       <body>
         <div id="root"></div>
         <script>
@@ -29,16 +32,21 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, '*');
+    // make sure the browser has enough time to update the srcdoc before posting message
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, '*');
+    }, 50);
   }, [code]);
 
   return (
-    <iframe
-      title="preview"
-      ref={iframe}
-      sandbox="allow-scripts"
-      srcDoc={html}
-    />
+    <div className="preview-wrapper">
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
   );
 };
 
